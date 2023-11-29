69b3dc89ff65b5b6d7f09b5a6f2764465079663d
LMutinda <lornamutinda3@gmail.com>
2023-11-29 12:15:47 +0300

    Hello world

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,9 +1,4 @@
-"""for i in range(100):
-    if i < 99:
-        #Print a leading zero for single-digit numbers
-        print("{:02d}".format(i), end=", ")
-    else:
-        print("{}".format(i)) """
+
 print(', '.join(["{:02d}".format(i) for i in range(100) ]))
 
 
