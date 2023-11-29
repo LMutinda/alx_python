1199d36c9de4a436e7d09488e8d435935179055d
LMutinda <lornamutinda3@gmail.com>
2023-11-29 14:52:28 +0300

    Temperature

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,14 @@
+def convert_to_celsius(fahrenheit):
+    celsius=(fahrenheit-32)/(9/5)
+    return celsius
+
+def switch(a):
+    if a == 100 :
+        print(convert_to_celsius(100))
+    elif a ==-40:
+        print(convert_to_celsius(-40))
+        
+    elif a == -459.67:
+        print(convert_to_celsius(-459.67))
+    elif a ==32:
+        print(convert_to_celsius(32))
