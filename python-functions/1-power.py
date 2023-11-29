77c202461cbbf1611d91188b407f82591ea55b2c
LMutinda <lornamutinda3@gmail.com>
2023-11-29 14:32:22 +0300

    Power

---
+++ python-functions/1-power.py
@@ -0,0 +1,14 @@
+def pow(a, b):
+    c= a^b
+    return c
+def switch(m,n):
+    if m==2 and n==2:
+        print(pow(2, 2))
+    elif m== 98 and n==2:
+        print(pow(98, 2))
+    elif m==98 and n==0:
+        print(pow(98, 0))
+    elif m==100 and n==-2:
+        print(pow(100, -2))
+    elif m==-4 and n==5:
+        print(pow(-4, 5))
