97698b5d3b424b04954ef7d538c336677fa44c3a
LMutinda <lornamutinda3@gmail.com>
2023-11-29 15:01:31 +0300

    Test

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -7,13 +7,15 @@ def switch(a):
         print(convert_to_celsius(100))
     elif a ==-40:
         print(convert_to_celsius(-40))
-        
     elif a == -459.67:
         print(convert_to_celsius(-459.67))
     elif a ==32:
         print(convert_to_celsius(32))
     elif a == 98.6:
         print(round(convert_to_celsius(98.6),1))
+
+print(round(convert_to_celsius(98.6),1))
+
 
 
 
