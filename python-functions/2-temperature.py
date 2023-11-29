9ea29490496517de7d0a6ebba0686fad27b994c2
LMutinda <lornamutinda3@gmail.com>
2023-11-29 16:26:03 +0300

    Functions

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,6 @@
 def convert_to_celsius(fahrenheit):
     celsius=(fahrenheit-32)*(5/9)
+    
     return celsius
 
 def switch(a):
@@ -8,7 +9,7 @@ def switch(a):
     elif a ==-40:
         print(convert_to_celsius(-40))
     elif a == -459.67:
-        print(round(convert_to_celsius(-459.67), 2))
+        print("%.2f" % convert_to_celsius(-459.67))
     elif a ==32:
         print(convert_to_celsius(32))
    
