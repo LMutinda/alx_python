b715e6fb273f9497732c73637e64ed5947c28537
LMutinda <lornamutinda3@gmail.com>
2023-11-29 14:21:33 +0300

    Switch

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -2,8 +2,25 @@ def add(a,b):
 
     c=a+b
     return c
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+
+def switch(m,n):
+    if m == 1 and n==2:
+        print(add(1, 2))
+    elif m==98 and n==0:
+        print(add(98, 0))
+    elif m==100 and n==-2:
+        print(add(100, -2))
+
+print(switch(1,2))
+print(switch(98,0))
+print(switch(100,-2))
+
+
+
+
+
+
+
+
 
 
