3d854d5c2d0b5d7fbb8ba53aacf45fbeb846a34e
LMutinda <lornamutinda3@gmail.com>
2023-11-29 15:15:01 +0300

    String

---
+++ python-functions/3-string.py
@@ -0,0 +1,12 @@
+def reverse_string(string):
+    txt = string[::-1]
+    return txt
+def switch(word):
+    if word == "Hello":
+        print(reverse_string("Hello"))
+    elif word == "":
+        print(reverse_string(""))
+    elif word == "madam":
+        print(reverse_string("madam"))
+    elif word == "Hello, World!":
+        print(reverse_string("Hello, World!"))
