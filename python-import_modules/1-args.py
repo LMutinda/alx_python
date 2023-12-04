67b6df3a000a28f5202f10726fd0e8aa2d12775d
LMutinda <lornamutinda3@gmail.com>
2023-12-04 12:31:06 +0300

    Args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    argv=[]
+    num= len(argv)
+    count = 0
+    if num == 1:
+        print("{} argument:".format(num))
+        for i in argv:
+            count = count + 1
+            print("{}: {}".format(count,i))
+    elif num == 0:
+        print("{} arguments.".format(num))
+        for i in argv:
+            count = count + 1
+            print("{}: {}".format(count,i))
+    else:
+        print("{} arguments:".format(num))
+        for i in argv:
+            count = count + 1
+            print("{}: {}".format(count,i))
+    
+
