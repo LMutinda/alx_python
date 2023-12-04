774281da355e2d6e865a3b2db01b1ee255a61d1f
LMutinda <lornamutinda3@gmail.com>
2023-12-04 13:01:47 +0300

    Import

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,8 @@ def safe_print_division(a, b):
         return None
     
     finally:
-        print("Inside result: {}".format(safe_print_division(a,b)))
+        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
 
 
 
