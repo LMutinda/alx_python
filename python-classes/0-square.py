cf8dde6e5db96eca9de92658cf5a6dee271289f2
LMutinda <lornamutinda3@gmail.com>
2023-12-10 12:52:03 +0300

    Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,18 @@
+"""
+This module consists of a class Square
+"""
 class Square:
+    """
+    This is the class Square. If the class has public attributes, they may be documented here
+    in an ``Attributes`` section.
+    """
     def __init__(self,size):
+        """
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+        Args:
+            size (int): Defines the size of the square
+        """
         self.__size = size
 
 
