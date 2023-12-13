957f3fd54fbf68837a095cc2964617942a21fcaf
LMutinda <lornamutinda3@gmail.com>
2023-12-13 20:11:00 +0300

    Inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,7 +1,29 @@
 """
-This is a module on inheritance
+This module contains an empty class
 """
-from Base import BaseGeometry
+
+class meta_class:
+    """This is the meta class"""
+    
+    def __dir__(cls):
+        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
+
+class BaseGeometry(meta_class):
+    
+    """This is a geometry class"""
+    def area(self):
+        raise Exception ("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if type(value) != int:
+            raise TypeError ("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError ("{} must be greater than 0".format(name))
+        
+
 
 class Rectangle (BaseGeometry):
     """This is a rectangle class that inherits from the BaseGeometry class"""
