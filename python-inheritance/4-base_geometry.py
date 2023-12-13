85057fd1c3ed04f743156d7100af34c9090a9ca2
LMutinda <lornamutinda3@gmail.com>
2023-12-13 19:02:27 +0300

    Inheritance

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,16 @@
+"""
+This module contains an empty class
+"""
+
+class meta_class:
+    """This is the meta class"""
+    
+    def __dir__(cls):
+        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
+
+class BaseGeometry(meta_class):
+    
+    """This is a geometry class"""
+    def area(self):
+        raise Exception ("area() is not implemented")
+       
