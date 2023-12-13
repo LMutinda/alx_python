f20a2fe919e7e4a80fb4ff82a4118fe4be5043a9
LMutinda <lornamutinda3@gmail.com>
2023-12-13 18:51:28 +0300

    Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,9 +1,16 @@
 """
 This module contains an empty class
 """
-class BaseGeometry:
-    """
-    This is an empty class
-    """
+
+class meta_class:
+    """This is the meta class"""
+    
+    def __dir__(cls):
+        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
+
+class BaseGeometry(meta_class):
+    
+    """This is an empty class"""
     pass
-  
+
+
