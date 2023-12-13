3d5588f4ac2f8ae9a0c222847cba41d1ac26f02c
LMutinda <lornamutinda3@gmail.com>
2023-12-13 20:59:03 +0300

    Inheritance

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -15,34 +15,23 @@ class BaseGeometry(meta_class):
         raise Exception ("area() is not implemented")
     
     def integer_validator(self, name, value):
-        self.name = "name"
-        
+        self.name = name
+        self.value = value
 
-        #if type(value) != int:
-            #raise TypeError ("{} must be an integer".format(name))
-        #if value <= 0:
-            #raise ValueError ("{} must be greater than 0".format(name))
-        if type(value) is int:
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
-            else:
-                self.value = value
-        else:
-            raise TypeError("{} must be an integer".format(name))
-
+        if type(value) != int:
+            raise TypeError ("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError ("{} must be greater than 0".format(name))
         
 
 
 class Rectangle (BaseGeometry):
     """This is a rectangle class that inherits from the BaseGeometry class"""
     def __init__(self, width, height):
-        
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-       
-
+        self.__width = width
+        self.__height = height
 
 
 
