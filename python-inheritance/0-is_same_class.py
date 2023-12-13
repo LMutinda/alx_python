20b0caefad876147555cffcac0691b960b849a58
LMutinda <lornamutinda3@gmail.com>
2023-12-13 17:57:42 +0300

    Inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -7,13 +7,19 @@ def is_same_class(obj, a_class):
     Method to determine whether an object is an instance of a class
 
     Args:
-        obj: instance
-        a_class: class name
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+    -True if obj is an instance of a_class, otherwise False.
+    
     """
-    if isinstance(obj,a_class) is True:
+    x= isinstance(obj, a_class)
+    if x == True and (obj != True and obj != None and obj != False ) :
         return True
-    else:
+    else: 
         return False
     
     
+
 
