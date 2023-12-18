c137e74d9ec4dcb3e1b1b04bca87f7bd54c48f1a
LMutinda <lornamutinda3@gmail.com>
2023-12-18 15:17:38 +0300

    Rect

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,27 +1,7 @@
 """
 This module contains an empty class
 """
-
-class meta_class:
-    """This is the meta class"""
-    
-    def __dir__(cls):
-        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
-
-class BaseGeometry(meta_class):
-    
-    """This is a geometry class"""
-    def area(self):
-        raise Exception ("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        self.name = name
-        self.value = value
-
-        if type(value) != int:
-            raise TypeError ("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError ("{} must be greater than 0".format(name))
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
         
 
 
