4b6648dc3169ad292007ab632af460067cba6d45
LMutinda <lornamutinda3@gmail.com>
2023-12-18 15:11:43 +0300

    Rect

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,42 +1,7 @@
 """
 This module contains an empty class
 """
-
-class meta_class:
-    """This is the meta class"""
-    
-    def __dir__(cls):
-        return [attribute for  attribute in super().__dir__() if attribute != "__init_subclass__"]
-
-class BaseGeometry(meta_class):
-    
-    """This is a geometry class"""
-    def area(self):
-        raise Exception ("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        self.name = name
-        self.value = value
-
-        if type(value) != int:
-            raise TypeError ("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError ("{} must be greater than 0".format(name))
-        
-
-
-class Rectangle (BaseGeometry):
-    """This is a rectangle class that inherits from the BaseGeometry class"""
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width*self.__height
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('7-rectangle').Rectangle
     
 class Square (Rectangle):
     """This is a square class that inherits from the Rectangle class"""
