f0a45eefbdd1f7efb0ceac282e37e75a3da2498c
LMutinda <lornamutinda3@gmail.com>
2023-12-21 16:30:51 +0300

    post

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+"""
+Take a user URL and an email address, send a POST request to the passed URL with the email as a parameter, and finally display the body of the response.
+"""
+
+import requests 
+import sys 
+
+url = sys.argv([1])
+email = sys.argv([2])
+
+response = requests.post(url,email)
+print(response.text)
