92f0379438600aa4b66572042d218e7bfd35df87
LMutinda <lornamutinda3@gmail.com>
2023-12-21 15:36:43 +0300

    network

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+"""
+Make a request to a server
+Request URL: https://alu-intranet.hbtn.io/status
+Request Method: GET
+"""
+import requests
+
+url = "https://alu-intranet.hbtn.io/status"
+
+response = requests.get(url)
+print("Body response:")
+print("     - type: {}".format(type(response.text)))
+print("     - content: {}".format(response.text))
