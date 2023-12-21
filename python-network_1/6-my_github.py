3cf1873f48994e2bb527ea0f002c1f97bc4265c5
LMutinda <lornamutinda3@gmail.com>
2023-12-21 18:34:19 +0300

    git

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,19 @@
+"""
+Write a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+
+    -You must use Basic Authentication with a personal access token as password to access to your information (only read:user permission is needed)
+    -The first argument will be your username
+    -The second argument will be your password (in your case, a personal access token as password)
+    -You must use the package requests and sys
+
+"""
+
+import requests 
+import sys 
+
+url = "https://docs.github.com/en/rest/users?apiVersion=2022-11-28"
+
+username = sys.argv[1]
+password = sys.argv [2]
+
+response = requests.post(url )
