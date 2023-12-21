645d3607bad5954cacf2f4de0ac3f24313888006
LMutinda <lornamutinda3@gmail.com>
2023-12-21 16:07:21 +0300

    GET

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+"""
+Take URL from user input, send request to URL and print value of variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+url = sys.argv[1]
+response = requests.get(url)
+print(response.headers)
