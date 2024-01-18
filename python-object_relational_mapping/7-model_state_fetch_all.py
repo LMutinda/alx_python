5a8ad80c518d247c554354cd82c958e05d3f081d
LMutinda <lornamutinda3@gmail.com>
2024-01-18 10:38:14 +0300

    fetch

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,33 +1,13 @@
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import mapper, relationship, Session
 from model_state import Base, State  
 
-def list_states(username, password, database):
-    # Create a SQLite engine (you can replace this with your MySQL connection)
-    engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@localhost:3306/{database}', echo=True)
-
-    # Bind the engine to the Base class
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
 
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query all State objects and sort by id
-    states = session.query(State).order_by(State.id).all()
-
-    # Display the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-if __name__ == "__main__":
-    # Check if all required arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1:]
-
-    # Call the function with provided arguments
-    list_states(username, password, database)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
